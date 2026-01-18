52c0ba92cd762609e3e5c54cf56587f356a4d4b2
kang <kaangaptx@outlook.com>
2026-01-18 14:19:05 +0800

    15.9 Find New Location Around Target

--- Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -1,13 +1,13 @@
 // CopyRight kang
 
 
-#include "AI/MyBTService_FindNearestPlayer.h"
+#include "AI/BTService_FindNearestPlayer.h"
 
 #include "AIController.h"
 #include "BehaviorTree/BTFunctionLibrary.h"
 #include "Kismet/GameplayStatics.h"
 
-void UMyBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
@@ -17,22 +17,19 @@ void UMyBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp,
 	TArray<AActor*> ActorsWithTag;
 	UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
 
-	for(AActor* Actor : ActorsWithTag)
-	{
-		GEngine->AddOnScreenDebugMessage(2, .5f, FColor::Orange, *Actor->GetName());
-	}
-
-
 	//遍历所有的角色获取到最近的角色和距离
 	float ClosestDistance = TNumericLimits<float>::Max(); //默认设置float最大值
 	AActor* ClosestActor = nullptr;
 	for(AActor* Actor : ActorsWithTag)
 	{
-		const float Distance = OwningPawn->GetDistanceTo(Actor);
-		if(Distance < ClosestDistance)
+		if(IsValid(Actor) && IsValid(OwningPawn))
 		{
-			ClosestDistance = Distance;
-			ClosestActor = Actor;
+			const float Distance = OwningPawn->GetDistanceTo(Actor);
+			if(Distance < ClosestDistance)
+			{
+				ClosestDistance = Distance;
+				ClosestActor = Actor;
+			}
 		}
 	}
 
